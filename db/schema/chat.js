06a64329a3bed7d6a820aa80bddfa42b3ee6c6a2
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-08-10 10:47:55 -0400

    feat: latest

--- db/schema/chat.js
+++ db/schema/chat.js
@@ -2,8 +2,8 @@ import { pgTable, serial, text, varchar, timestamp } from "drizzle-orm/pg-core";
 
 export const chat = pgTable('chat', {
   id: serial('id').primaryKey(),
-  role: text('role'), //json format of the AI that responds
-  content: text('content'),
+  role: text('role').notNull(), //json format of the AI that responds
+  content: text('content').notNull(),
   createdAt: timestamp('createdAt', {withTimezone:true}).defaultNow().notNull(),
 });
 
