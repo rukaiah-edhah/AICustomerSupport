d2b3a959d885e125e3c29f1ad014e6b8e165061d
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-08-10 18:42:12 -0400

    fix: npm legacy peer deps

--- db/schema/chat.js
+++ db/schema/chat.js
@@ -2,6 +2,7 @@ import { pgTable, serial, text, varchar, timestamp } from "drizzle-orm/pg-core";
 
 export const chat = pgTable('chat', {
   id: serial('id').primaryKey(),
+  user_id: text('user_id').notNull(),
   role: text('role').notNull(), //json format of the AI that responds
   content: text('content').notNull(),
   createdAt: timestamp('createdAt', {withTimezone:true}).defaultNow().notNull(),
