f68e6816b9c7be5072a8aa042396da056720766c
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-08-10 22:45:50 -0400

    fix: updated schema

--- db/schema/chat.js
+++ db/schema/chat.js
@@ -1,8 +1,9 @@
-import { pgTable, serial, text, varchar, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
 
 export const chat = pgTable('chat', {
   id: serial('id').primaryKey(),
   user_id: text('user_id'),
+  chat_id: integer('chat_id'),
   role: text('role').notNull(), //json format of the AI that responds
   content: text('content').notNull(),
   createdAt: timestamp('createdAt', {withTimezone:true}).defaultNow().notNull(),
